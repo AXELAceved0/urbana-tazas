9bc88ae6437b319509f18ab535ddbaebfcda91c1
Axel Acevedo <axelacevedo021@gmail.com>
2023-11-27 22:40:32 -0300

    menu

---
+++ main.js
@@ -0,0 +1,6 @@
+//let nombre = prompt("como te llamas");
+//let apellido = prompt("cual es tu apellido");
+//let edad = Number(prompt("caul es tu edad"));
+//let trabajo = prompt("cual es tu trabajo");
+
+//console.log("Me llamo " +  nombre + " " + apellido +  " tengo " + " " + edad + " años " + " trabajo de " + trabajo + " ");
